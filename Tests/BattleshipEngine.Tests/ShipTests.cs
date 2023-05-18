0e5e138aaec26c9056184848de94bba13d9b1a07
smabuk <2011834+smabuk@users.noreply.github.com>
2023-05-18 11:22:34 +0100

    More refactoring

--- Tests/BattleshipEngine.Tests/ShipTests.cs
+++ Tests/BattleshipEngine.Tests/ShipTests.cs
@@ -3,26 +3,26 @@
 public sealed class ShipTests
 {
 	[Theory]
-	[InlineData(ShipType.Destroyer,  Orientation.Horizontal, "A1", "A2", 2)]
-	[InlineData(ShipType.Cruiser,    Orientation.Horizontal, "A2", "A4", 3)]
-	[InlineData(ShipType.Submarine,  Orientation.Horizontal, "A1", "A3", 3)]
-	[InlineData(ShipType.Battleship, Orientation.Horizontal, "B6", "B9", 4)]
-	[InlineData(ShipType.AircraftCarrier,    Orientation.Horizontal, "A1", "A5", 5)]
-	[InlineData(ShipType.Destroyer,  Orientation.Vertical,   "A2", "B2", 2)]
-	[InlineData(ShipType.Cruiser,    Orientation.Vertical,   "A1", "C1", 3)]
-	[InlineData(ShipType.Submarine,  Orientation.Vertical,   "A1", "C1", 3)]
-	[InlineData(ShipType.Battleship, Orientation.Vertical,   "B6", "E6", 4)]
-	[InlineData(ShipType.AircraftCarrier,    Orientation.Vertical,   "A1", "E1", 5)]
+	[InlineData(ShipType.Destroyer,        Orientation.Horizontal, "A1", "A2", 2)]
+	[InlineData(ShipType.Cruiser,          Orientation.Horizontal, "A2", "A4", 3)]
+	[InlineData(ShipType.Submarine,        Orientation.Horizontal, "A1", "A3", 3)]
+	[InlineData(ShipType.Battleship,       Orientation.Horizontal, "B6", "B9", 4)]
+	[InlineData(ShipType.AircraftCarrier,  Orientation.Horizontal, "A1", "A5", 5)]
+	[InlineData(ShipType.Destroyer,        Orientation.Vertical,   "A2", "B2", 2)]
+	[InlineData(ShipType.Cruiser,          Orientation.Vertical,   "A1", "C1", 3)]
+	[InlineData(ShipType.Submarine,        Orientation.Vertical,   "A1", "C1", 3)]
+	[InlineData(ShipType.Battleship,       Orientation.Vertical,   "B6", "E6", 4)]
+	[InlineData(ShipType.AircraftCarrier,  Orientation.Vertical,   "A1", "E1", 5)]
 	public void ShipValidWhenCreated(ShipType shipType, Orientation orientation, string startPosition, string lastPosition, int expectedSegmentCount)
 	{
 		Ship actual = new Ship(shipType, startPosition, orientation);
 	
-		Assert.Equal(orientation, actual.Orientation);
-		Assert.Equal(shipType, actual.Type);
-		Assert.Equal(startPosition, actual.Position);
+		Assert.Equal(orientation,          actual.Orientation);
+		Assert.Equal(shipType,             actual.Type);
+		Assert.Equal(startPosition,        actual.Position);
 		Assert.Equal(expectedSegmentCount, actual.Segments.Count);
-		Assert.Equal(startPosition, actual.Segments.First().Key);
-		Assert.Equal(lastPosition, actual.Segments.Last().Key);
+		Assert.Equal(startPosition,        actual.Segments.First().Key);
+		Assert.Equal(lastPosition,         actual.Segments.Last().Key);
 	}
 
 	[Fact]
--- src/BSMConsole/BattleshipGame.cs
+++ src/BSMConsole/BattleshipGame.cs
@@ -7,23 +7,23 @@ internal class BattleshipGame
 	public bool RandomShipPlacement { get; set; } = false;
 	public GameType GameType { get; set; } = GameType.Classic;
 
-	private const int ONE_MINUTE = 60000;
+	// Game layout constants
 	private const int LEFT_GRID = 4;
 	private const int RIGHT_GRID = 34;
 	private const int BOARD_ROW = 2;
 	private const int GAME_HEIGHT = 22;
 	private const int GAME_WIDTH = 66;
 	private const int STATUS_ROW = 17;
+	private const int INPUT_COL = 2;
+	private const int INPUT_ROW = 18;
 	private const int CLEAR_WIDTH = 56;
 
-	private int _bottomRow;
 	private int _topRow = int.MinValue;
-	private int _inputRow;
 
 	private readonly Dictionary<Coordinate, AttackResult> shots = new();
 	private Dictionary<ShipType, Ship> myFleet = new();
 
-	private PrivatePlayer human = new("Me");
+	private PrivatePlayer player = new("Me");
 	private Player opponent = new("Computer", IsComputer: true);
 
 	internal void Play()
@@ -32,8 +32,10 @@ internal class BattleshipGame
 
 		GameStatus gameStatus = GameStatus.AddingPlayers;
 
-		human = game.AddPlayer(PlayerName);
+		player = game.AddPlayer(PlayerName);
 		opponent = Player.PublicPlayer(game.AddPlayer("Computer", isComputer: true));
+
+		_topRow = PrepareGameSpace();
 
 		DisplayGame();
 
@@ -41,10 +43,10 @@ internal class BattleshipGame
 		DisplayStatus(gameStatus);
 
 		if (RandomShipPlacement) {
-			game.PlaceShips(human, doItForMe: true);
-			myFleet = game.Fleet(human).ToDictionary(ship => ship.Type, ship => ship);
+			game.PlaceShips(player, doItForMe: true);
+			myFleet = game.Fleet(player).ToDictionary(ship => ship.Type, ship => ship);
 		} else {
-			DisplayGrid(human);
+			DisplayGrid(player);
 			PlaceShips();
 		}
 
@@ -52,13 +54,13 @@ internal class BattleshipGame
 
 			gameStatus = GameStatus.Attacking;
 			DisplayStatus(gameStatus);
-			DisplayBoards(human, opponent);
+			DisplayBoards(player, opponent);
 
 			while (game.GameOver is false) {
-				if (TryGetCoordinateFromUser(_inputRow, out Coordinate coordinate)) {
-					shots.TryAdd(coordinate, game.Fire(human, coordinate));
+				if (TryGetCoordinateFromUser(_topRow + INPUT_ROW, out Coordinate coordinate)) {
+					shots.TryAdd(coordinate, game.Fire(player, coordinate));
 					List<AttackResult> attackResults = game.OtherPlayersFire().ToList();
-					DisplayBoards(human, opponent);
+					DisplayBoards(player, opponent);
 				} else {
 					gameStatus = GameStatus.Abandoned;
 					break;
@@ -75,29 +77,19 @@ internal class BattleshipGame
 		void DisplayBoards(PrivatePlayer player, Player opponent)
 		{
 			DisplayGrid(opponent);
-			DisplayGridContents(opponent);
+			DisplayShotsOnGrid(opponent);
+
 			DisplayGrid(player, offsetCol: RIGHT_GRID);
-			DisplayGridContents(player, offsetCol: RIGHT_GRID);
+			DisplayShipsOnGrid(myFleet.Values, RIGHT_GRID);
 		}
 
 		void DisplayGame()
 		{
-			if (_topRow == int.MinValue) {
-				for (int i = 0; i < GAME_HEIGHT; i++) {
-					Console.WriteLine();
-				}
-
-				(int _, _bottomRow) = Console.GetCursorPosition();
-				_topRow = _bottomRow - GAME_HEIGHT;
-				(_, _bottomRow) = Console.GetCursorPosition();
-				_inputRow = _bottomRow - 4;
-			}
-
 			Console.SetCursorPosition(0, _topRow);
 			Console.Write($"┌{new string('─', GAME_WIDTH - 2)}┐");
 			Console.WriteLine();
 			for (int row = 0; row < GAME_HEIGHT - 4; row++) {
-				Console.Write($"|{new string(' ', GAME_WIDTH - 2)}|");
+				Console.Write($"│{new string(' ', GAME_WIDTH - 2)}│");
 				Console.WriteLine();
 			}
 			Console.Write($"└{new string('─', GAME_WIDTH - 2)}┘");
@@ -107,9 +99,9 @@ internal class BattleshipGame
 			Console.Write($" T H E   G A M E   O F   B A T T L E S H I P ");
 		}
 
-		void DisplayGrid(Player? player = null, int offsetCol = LEFT_GRID)
+		void DisplayGrid(Player player, int offsetCol = LEFT_GRID)
 		{
-			const string SEA_COLOUR = "blue";
+			const string SEA_COLOUR = "blue on black";
 			const char SEA = '.';
 			int boardSize = game.BoardSize;
 			int offsetRow = _topRow + BOARD_ROW;
@@ -126,25 +118,24 @@ internal class BattleshipGame
 				Console.Write($"{Convert.ToChar(row + 'A'),2} │ ");
 				string sea = string.Join(" ", Enumerable.Repeat($"{SEA}", boardSize));
 				AnsiConsole.Markup($"[{SEA_COLOUR}]{sea}[/]");
-				Console.Write(" |");
+				Console.Write(" │");
 			}
 			Console.SetCursorPosition(offsetCol, offsetRow + 13);
 			Console.Write("   └─────────────────────┘");
 		}
 
-		void DisplayGridContents(Player? player = null, int offsetCol = LEFT_GRID)
+		void DisplayShipsOnGrid(IEnumerable<Ship>? ships, int offsetCol = LEFT_GRID)
 		{
 			const string HIT_COLOUR = "red";
-			const string MISS_COLOUR = "blue";
 			const string HIT = $"[{HIT_COLOUR}]x[/]";
-			const string SUNK = $"[{HIT_COLOUR}]X[/]";
+			const string MISS_COLOUR = "blue";
 			const string MISS = $"[{MISS_COLOUR}]O[/]";
 
 			int offsetRow = _topRow + BOARD_ROW;
 
 			// Display ships on the board
-			if (player is PrivatePlayer) {
-				foreach (Ship ship in myFleet.Values) {
+			if (ships is not null) {
+				foreach (Ship ship in ships) {
 					foreach (ShipSegment segment in ship.Segments.Values) {
 						Console.SetCursorPosition(offsetCol + 3 + (segment.Coordinate.Col * 2), offsetRow + 2 + segment.Coordinate.Row);
 						string hitormiss = segment.IsHit
@@ -154,27 +145,37 @@ internal class BattleshipGame
 						AnsiConsole.Markup(hitormiss);
 					}
 				}
-			} else {
-				foreach (AttackResult shot in shots.Values.Where(s => s.TargetedPlayer == player)) {
-					Console.SetCursorPosition(offsetCol + 3 + (shot.AttackCoordinate.Col * 2), offsetRow + 2 + shot.AttackCoordinate.Row);
-					bool sunk = shots.Values.Any(s => s.ShipType == shot.ShipType && s.HitOrMiss == AttackResultType.HitAndSunk);
-					string hitormiss = shot.HitOrMiss switch
-					{
-						AttackResultType.Miss => MISS,
-						AttackResultType.Hit => sunk ? $"[{HIT_COLOUR}]{GetShipShape(shot.ShipType).ToUpper()}[/]" : $"[{HIT_COLOUR}]{GetShipShape(shot.ShipType)}[/]",
-						AttackResultType.HitAndSunk => $"[{HIT_COLOUR}]{GetShipShape(shot.ShipType).ToUpper()}[/]",
-						AttackResultType.AlreadyAttacked => throw new ArgumentOutOfRangeException(nameof(shot.HitOrMiss)),
-						AttackResultType.InvalidPosition => throw new ArgumentOutOfRangeException(nameof(shot.HitOrMiss)),
-						_ => throw new ArgumentOutOfRangeException(nameof(shot.HitOrMiss)),
-					};
-					AnsiConsole.Markup(hitormiss);
-				}
+			}
+		}
+
+		void DisplayShotsOnGrid(Player player, int offsetCol = LEFT_GRID)
+		{
+			const string HIT_COLOUR = "red on black";
+			const string SUNK_COLOUR = $"red on black";
+			const string MISS_COLOUR = "blue on black";
+			const string MISS = $"[{MISS_COLOUR}]O[/]";
+
+			int offsetRow = _topRow + BOARD_ROW;
+
+			foreach (AttackResult shot in shots.Values.Where(s => s.TargetedPlayer == player)) {
+				Console.SetCursorPosition(offsetCol + 3 + (shot.AttackCoordinate.Col * 2), offsetRow + 2 + shot.AttackCoordinate.Row);
+				bool sunk = shots.Values.Any(s => s.ShipType == shot.ShipType && s.HitOrMiss == AttackResultType.HitAndSunk);
+				string hitormiss = shot.HitOrMiss switch
+				{
+					AttackResultType.Miss => MISS,
+					AttackResultType.Hit => sunk ? $"[{SUNK_COLOUR}]{GetShipShape(shot.ShipType).ToUpper()}[/]" : $"[{HIT_COLOUR}]{GetShipShape(shot.ShipType)}[/]",
+					AttackResultType.HitAndSunk => $"[{SUNK_COLOUR}]{GetShipShape(shot.ShipType).ToUpper()}[/]",
+					AttackResultType.AlreadyAttacked => throw new ArgumentOutOfRangeException(nameof(shot.HitOrMiss)),
+					AttackResultType.InvalidPosition => throw new ArgumentOutOfRangeException(nameof(shot.HitOrMiss)),
+					_ => throw new ArgumentOutOfRangeException(nameof(shot.HitOrMiss)),
+				};
+				AnsiConsole.Markup(hitormiss);
 			}
 		}
 
 		void DisplayFinalSummary()
 		{
-			Console.SetCursorPosition(0, _inputRow + 1);
+			Console.SetCursorPosition(0, _topRow + GAME_HEIGHT - 2);
 			Console.WriteLine();
 			Console.WriteLine();
 			Console.WriteLine($" Results");
@@ -204,51 +205,69 @@ internal class BattleshipGame
 
 		void PlaceShips()
 		{
-			myFleet = game.Fleet(human).ToDictionary(ship => ship.Type);
+			myFleet = game.Fleet(player).ToDictionary(ship => ship.Type);
 
-			DisplayGridContents(human);
+			DisplayShipsOnGrid(myFleet.Values);
 
-			List<Ship> fleet = game.Fleet(human).Where(ship => ship.IsPositioned == false).ToList();
+			List<Ship> fleet = game.Fleet(player).Where(ship => ship.IsPositioned == false).ToList();
 
 			foreach (Ship ship in fleet) {
 				Ship newShip;
 				do {
-					DisplayGridContents(human);
+					DisplayShipsOnGrid(myFleet.Values);
 					DisplayStatus(GameStatus.PlacingShips, $" [green]{ship.Type.ToFriendlyString()}[/] ({ship.NoOfSegments} segments) ");
-					(Orientation Orientation, Coordinate Coordinate)? result = GetShipPlacementFromUser(_inputRow);
+					(Orientation Orientation, Coordinate Coordinate)? result = GetShipPlacementFromUser(_topRow + INPUT_ROW);
 
 					if (!result.HasValue) {
 						return;
 					}
 					newShip = new(ship.Type, result.Value.Coordinate, result.Value.Orientation);
 
-				} while (!game.PlaceShip(human, newShip));
+				} while (!game.PlaceShip(player, newShip));
 				myFleet[newShip.Type] = newShip;
 			}
 		}
 	}
 
+	private int PrepareGameSpace()
+	{
+		for (int i = 0; i < GAME_HEIGHT; i++) {
+			Console.WriteLine();
+		}
+
+		(_, int bottomRow) = Console.GetCursorPosition();
+		return bottomRow - GAME_HEIGHT;
+	}
 
 	private static bool TryGetCoordinateFromUser(int inputRow, out Coordinate coordinate)
 	{
 		string currentCoordinateString = "";
 
 		while (true) {
-			ConsoleKey key = DisplayAndGetInput(inputRow, currentCoordinateString, "     Target coordinates: ");
-			if (key == ConsoleKey.Escape) {
-				coordinate = default;
-				return false;
-			} else if (key == ConsoleKey.Enter && currentCoordinateString.Length > 1) {
-				coordinate = new Coordinate(currentCoordinateString);
-				return true;
-			} else if (key == ConsoleKey.Backspace && currentCoordinateString.Length > 0) {
-				currentCoordinateString = currentCoordinateString[..^1];
-			} else if (key >= ConsoleKey.A && key <= ConsoleKey.J && currentCoordinateString.Length == 0) {
-				currentCoordinateString += key;
-			} else if (key >= ConsoleKey.D1 && key <= ConsoleKey.D9 && currentCoordinateString.Length == 1) {
-				currentCoordinateString += key.ToString()[^1];
-			} else if (key == ConsoleKey.D0 && currentCoordinateString[1] == '1') {
-				currentCoordinateString += key.ToString()[^1];
+			ConsoleKey key = DisplayAndGetInput(INPUT_COL, inputRow, CLEAR_WIDTH, $"[bold]{currentCoordinateString}[/]", "     Target coordinates: ");
+			switch (key) {
+				case ConsoleKey.Escape:
+					coordinate = default;
+					return false;
+				case ConsoleKey.Enter when currentCoordinateString.Length > 1:
+					coordinate = new Coordinate(currentCoordinateString);
+					return true;
+
+				case ConsoleKey.Backspace when currentCoordinateString.Length > 0:
+					currentCoordinateString = currentCoordinateString[..^1];
+					break;
+				case >= ConsoleKey.A and <= ConsoleKey.J when currentCoordinateString.Length == 0:
+					currentCoordinateString += key;
+					break;
+				case (>= ConsoleKey.D1 and <= ConsoleKey.D9)
+				  or (>= ConsoleKey.NumPad1 and <= ConsoleKey.NumPad9) when currentCoordinateString.Length == 1:
+					currentCoordinateString += key.ToString()[^1];
+					break;
+				case ConsoleKey.D0 or ConsoleKey.NumPad0 when currentCoordinateString.Length == 2 && currentCoordinateString[1] == '1':
+					currentCoordinateString += key.ToString()[^1];
+					break;
+				default:
+					break;
 			}
 		}
 	}
@@ -259,22 +278,36 @@ internal class BattleshipGame
 		Orientation orientation = Orientation.Horizontal;
 
 		while (true) {
-			ConsoleKey key = DisplayAndGetInput(inputRow, currentCoordinateString, $" Position ({orientation,9}): ");
-			if (key == ConsoleKey.Escape) {
-				return null;
-			} else if (key == ConsoleKey.Enter && currentCoordinateString.Length > 1) {
-				Coordinate coordinate = new Coordinate(currentCoordinateString);
-				return (orientation, coordinate);
-			} else if (key == ConsoleKey.Backspace && currentCoordinateString.Length > 0) {
-				currentCoordinateString = currentCoordinateString[..^1];
-			} else if (key >= ConsoleKey.A && key <= ConsoleKey.J && currentCoordinateString.Length == 0) {
-				currentCoordinateString += key;
-			} else if (key >= ConsoleKey.D1 && key <= ConsoleKey.D9 && currentCoordinateString.Length == 1) {
-				currentCoordinateString += key.ToString()[^1];
-			} else if (key is ConsoleKey.LeftArrow or ConsoleKey.RightArrow) {
-				orientation = Orientation.Horizontal;
-			} else if (key is ConsoleKey.UpArrow or ConsoleKey.DownArrow) {
-				orientation = Orientation.Vertical;
+			ConsoleKey key = DisplayAndGetInput(INPUT_COL, inputRow, CLEAR_WIDTH, $"[bold]{currentCoordinateString}[/]", $" Position ({orientation,9}): ");
+			switch (key) {
+				case ConsoleKey.Escape:
+					return null;
+				case ConsoleKey.Enter when currentCoordinateString.Length > 1: {
+						Coordinate coordinate = new Coordinate(currentCoordinateString);
+						return (orientation, coordinate);
+					}
+
+				case ConsoleKey.Backspace when currentCoordinateString.Length > 0:
+					currentCoordinateString = currentCoordinateString[..^1];
+					break;
+				case >= ConsoleKey.A and <= ConsoleKey.J when currentCoordinateString.Length == 0:
+					currentCoordinateString += key;
+					break;
+				case (>= ConsoleKey.D1 and <= ConsoleKey.D9)
+				  or (>= ConsoleKey.NumPad1 and <= ConsoleKey.NumPad9) when currentCoordinateString.Length == 1:
+					currentCoordinateString += key.ToString()[^1];
+					break;
+				case ConsoleKey.D0 or ConsoleKey.NumPad0 when currentCoordinateString.Length == 2 && currentCoordinateString[1] == '1':
+					currentCoordinateString += key.ToString()[^1];
+					break;
+				case ConsoleKey.LeftArrow or ConsoleKey.RightArrow:
+					orientation = Orientation.Horizontal;
+					break;
+				case ConsoleKey.UpArrow or ConsoleKey.DownArrow:
+					orientation = Orientation.Vertical;
+					break;
+				default:
+					break;
 			}
 		}
 	}
@@ -282,15 +315,15 @@ internal class BattleshipGame
 	private static string GetShipShape(ShipType? shipType) =>
 		shipType?.ToString()[0].ToString().ToLowerInvariant() ?? "S";
 
-	private static ConsoleKey DisplayAndGetInput(int row, string input, string message = "Press <Esc> to exit ... ")
+	private static ConsoleKey DisplayAndGetInput(int col, int row, int clearCols, string input, string message = "Press <Esc> to exit ... ")
 	{
-		Console.SetCursorPosition(2, row);
-		Console.Write(new string(' ', CLEAR_WIDTH));
+		const int ONE_MINUTE = 60000;
 
-		Console.SetCursorPosition(2, row);
+		Console.SetCursorPosition(col, row);
+		Console.Write(new string(' ', clearCols));
 
-		Console.ResetColor();
-		Console.Write($" {message}{input}");
+		Console.SetCursorPosition(col, row);
+		AnsiConsole.Markup($" {message}{input}");
 
 		// If we get a timeout return a key that we don't use (Zoom)
 		return KeyReader.ReadKey(ONE_MINUTE) ?? ConsoleKey.Zoom;
