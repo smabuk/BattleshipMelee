98a96b46121736b702cfd8794f2029b6883f7045
smabuk <2011834+smabuk@users.noreply.github.com>
2023-05-19 12:46:44 +0100

    Add SignalR Hub

---
+++ src/BattleshipMelee.Server/Hubs/GameHub.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleshipMelee.Server.Hubs;
+
+internal class GameHub : Hub
+{
+	public override Task OnConnectedAsync()
+	{
+		return base.OnConnectedAsync();
+	}
+
+	public override Task OnDisconnectedAsync(Exception? exception)
+	{
+		return base.OnDisconnectedAsync(exception);
+	}
+}
---
+++ src/BattleshipMelee.Server/Usings.cs
@@ -0,0 +1,5 @@
+﻿global using System.Runtime.CompilerServices;
+
+global using Microsoft.AspNetCore.SignalR;
+
+global using BattleshipMelee.Server.Hubs;
